8df128d6b0978628530ce134deb1016bd59f8de1
vibeofficial <ichioguchristopher98@gmail.com>
2025-12-15 10:29:05 +0100

    retouch

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('./config/mongoDB');
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const swagger = require('./utils/swagger');
 const swagger_UI = require("swagger-ui-express");
 const errorHandler = require('./middleware/errorHandler');
@@ -9,9 +11,16 @@ const userRouter = require('./routers/user');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
 app.use(checkDBConnection);
 
 app.use('/api/v1', userRouter);
 app.use("/docs", swagger_UI.serve, swagger_UI.setup(swagger.openapiSpecification));
+
+app.use((req, res, next) => {
+  next(new appError(`Can't find ${req.originalUrl} on this server`, 404))
+});
+
 app.use(errorHandler);
 module.exports = app;
